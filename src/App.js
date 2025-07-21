1b284dfec4f60242b0f3a74992db50d8a3fb8c7a
Udyotha Sankalpana <s.udyotha@gtngroup.com>
2025-07-21 15:29:39 +0530

    Initial Changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Grid from './Grid';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Ag-Grid Table with Pagination</h1> */}
+      <Grid />
+    </div>
+  );
+}
+
+export default App;
